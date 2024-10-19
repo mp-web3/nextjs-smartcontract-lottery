e15e42d1bd5ad080c2d830c4e9688ac7178b6724
mp-web3 <mp.web3.t@gmail.com>
2024-10-19 05:54:36 +0200

    getting entranceFee from deployed contract and displaying it on frontend (work in progress)

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -1,17 +1,67 @@
 import { useWeb3Contract } from "react-moralis"
+import { contractAddresses, abi } from "@/constants/constants"
+import { useMoralis } from "react-moralis"
+import { useEffect } from "react"
 
 export default function LotteryEntrance() {
+    // Moralis know which chain we are on, because the Header passes up
+    // to the MoralisProvider (_app.js) all of the informations about
+    // metamask.
+    // Moralis Provider than passes it down to all of the components inside Moralis
+    // provider tags <MoralisProvider><Component {...pageProps}/></MoralisProvider>
+
+    // Motalis is going to give us the "0x...", we need to remove the "0x"
+    // Pull out the chainIdHex object and rename it chainId
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+    // console.log(parseInt(chainIdHex))
+    const chainId = parseInt(chainIdHex, 16) //convert hex to integer
+    const raffleAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
+    // let entranceFee
+    // entranceFee must be a hook, otherwise when it gets updated is frontend does not rerender and we do not see it!
+    // runContractFunction can both send transactions and read state
+    // -----------------------------------------------
     // const { runContractFunction: enterRaffle } = useWeb3Contract({
-    //     abi: ,
-    //     contractAddress: ,
-    //     functionName: ,
+    //     abi: abi,
+    //     contractAddress: raffleAddress,
+    //     functionName: enterRaffle,
     //     params: {},
-    //     msgValue:
+    //     msgValue: entranceFee,
     // })
+
+    // When frontend loads we are going to read the entranceFee (which is set dinamically when Raffle.sol is deployed)
+    // By using calling the function getEntranceFee()
+    // Check if raffleAddress is valid
+    console.log("Raffle Address:", raffleAddress)
+
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "getEntranceFee",
+        params: {},
+    })
+
+    useEffect(() => {
+        if (isWeb3Enabled && raffleAddress) {
+            // Ensure Web3 is enabled and raffleAddress is valid
+            async function readEntranceFee() {
+                try {
+                    entranceFee = (await getEntranceFee()).toString()
+                    console.log("Entrance Fee:", entranceFee) // Log entrance fee
+                } catch (error) {
+                    console.error("Error fetching entrance fee:", error) // Catch and log any errors
+                }
+            }
+            readEntranceFee()
+        } else {
+            console.log(
+                `Web3 must be true: ${isWeb3Enabled}. raffleAddress cannot be null: ${raffleAddress}`
+            )
+        }
+    }, [isWeb3Enabled, raffleAddress])
 
     return (
         <div>
-            <p>Here is where you'll enter the lottery!</p>
+            <p>Hi frome lottery! The fee in ETH to participate is {entranceFee}</p>
         </div>
     )
 }
