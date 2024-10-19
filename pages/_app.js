02ec40bf31d1b3efa4587fbde17989e5f8ae04fd
mp-web3 <mp.web3.t@gmail.com>
2024-10-19 08:14:12 +0200

    imported NotificationProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
-import "@/styles/globals.css";
-import { MoralisProvider } from "react-moralis";
+import "@/styles/globals.css"
+import { MoralisProvider } from "react-moralis"
+import { NotificationProvider } from "@web3uikit/core"
 
 export default function App({ Component, pageProps }) {
-  return (
-    // The initializeOnMount is the option to hook into a server to access more functionalities
-    <MoralisProvider initializeOnMount={false}>
-      <Component {...pageProps} />;
-    </MoralisProvider>
-  );
+    return (
+        // The initializeOnMount is the option to hook into a server to access more functionalities
+        <MoralisProvider initializeOnMount={false}>
+            <NotificationProvider>
+                <Component {...pageProps} />;
+            </NotificationProvider>
+        </MoralisProvider>
+    )
 }
